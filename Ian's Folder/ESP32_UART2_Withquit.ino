44900c64747b7f8463a0b1edba2b7f97f86eda13
Ian McGuire <ism360949@tamu.edu>
2026-02-22 15:19:08 -0600

    Update ESP32_UART2_Withquit.ino

--- Ian's Folder/ESP32_UART2_Withquit.ino
+++ Ian's Folder/ESP32_UART2_Withquit.ino
@@ -108,7 +108,7 @@ const float US_TRIM_FRACTION = 0.20f;
 //            INVENTORY THRESHOLD (TUNABLE, CM)
 // ============================================================
 
-const float INVENTORY_THRESH_CM = 11.9f;
+const float INVENTORY_THRESH_CM = 12.0f;
 
 // ============================================================
 //                      UART1 (JETSON) SETUP
